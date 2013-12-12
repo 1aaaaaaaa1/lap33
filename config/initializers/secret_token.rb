487482115b2383e3daa7bd0489e00f7a30bedc15
1aaaaaaaa1 <qctrai@yahoo.com.vn>
2013-12-12 10:06:25 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lap33::Application.config.secret_key_base = '088e6b97509be602ecc31e776312783754ad12e31d4441d222898f3322e23daacaa0b40492bddfbdb95fc5bc1f64cb0f3f3f83acbd0b1ce23d2eaff8cddd93c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lap33::Application.config.session_store :cookie_store, key: '_lap33_session'
